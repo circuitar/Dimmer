13a48dc5d75fe0294655d6aacc0553e85a0efb40
Alex Monteiro Sartin <sartinalex@gmail.com>
2015-07-10 13:54:10 -0300

    Dimmer multi-instance library update. Library ready to be tested. Example: DisplayLamps.ino adapted to the new library. Comments ready for python script.

--- Dimmer.h
+++ Dimmer.h
@@ -1,70 +1,133 @@
-/*
- This is the Dimmer library to control the AC load power and also dimmerable lamps
- 
- Copyright (c) 2015 Circuitar
- This software is released under the MIT license. See the attached LICENSE file for details.
+/** @file
+ * This is the Dimmer library to control the AC load power and also dimmerable lamps.
+ *
+ * Version 2.0 - Multi-instance objects.
+ *
+ * Copyright (c) 2015 Circuitar
+ * This software is released under the MIT license. See the attached LICENSE file for details.
  */
-
 #ifndef DIMMER_H
 #define DIMMER_H
 
 #include "Arduino.h"
-#include <TimerOne.h>
 
-//Define Zero cross pin and interrupt here
+/**
+ * Maximum number of triacs that can be used. This is also the default value.
+ */
+#define MAX_TRIAC 10
+
+/**
+ * NanoShield_zeroCross pin settings.
+ *
+ * @param zeroCrossPin	Change this parameter to the pin your zero cross is attached.
+ * @param zeroCrossInt	Change this parameter to the interruption correspondent to the pin of the zero cross. 
+ * @see https://www.arduino.cc/en/Reference/attachInterrupt for more information.
+ */
 #define zeroCrossPin    2
 #define zeroCrossInt    0
 
-#define MAX_TRIAC 10
-
- //Possible operation modes
+/**
+ * Possible operation modes for the dimmer library.
+ */
 #define NORMAL_MODE     0
 #define RAMP_MODE       1
 #define COUNT_MODE      2
 
- //Possible light state
+/**
+ * Possible light state for the dimmer library.
+ */
 #define OFF    0
 #define ON     1
 
-    
-class DimmerControl{
-    
-public:
-    DimmerControl(uint8_t triacPin, uint8_t value=50, bool state = ON, uint8_t resolution = 1);
-    bool begin(uint8_t mode = NORMAL_MODE);
+/**
+ * A ZeroCross Nanoshield or similar module to dimm AC lights.
+ */
+class DimmerControl
+{    
+  public:
+  	/**
+     * Constructor.
+     *
+     * Creates an object to access one Triac Nanoshield.
+     * @param triacPin	pin select matching the jumper on the board. (D3, D5, etc)
+     * @param value 	initial intensity in percentage of the dimm light.
+     *					Minimum is 0 and maximum is 100. Defaut is 50%.
+     * @param state 	initial state of the light. Possible states: ON or OFF. Default is ON.
+     * @see   begin()
+     */ 
+    DimmerControl(uint8_t triacPin, uint8_t value=50, bool state = ON);
+
+    /**
+     * Initializes the module.
+     *
+     * Initializes ZeroCross and Timer 2 interrupts. Set the light according to initial settings.
+     * @param mode 		operation mode to crontrol the light. 
+     *					Possible modes:
+     *					NORMAL_MODE: Uses timer apply only a percentage of the AC power to the light.
+     *					RAMP_MODE: Same as in the normal mode, but it applies a ramp effect to the light.
+     *					COUNT_MODE: Counts AC waves and apply full wave in time to time. @see resolution
+	 * @param resolution 	divided prescalar to define how many pulses to count in COUNT_MODE.
+	 *						if resolution is 1 the mode counts 100 pulses, if resolution is 2
+	 *						the mode counts 50 pulses, and so on.
+	 *
+     */
+    bool begin(uint8_t mode = NORMAL_MODE, uint8_t resolution = 1);
+
+    /**
+     * Turns the light OFF. 
+     */
     void off();
+
+    /**
+     * Turns the light ON. 
+     */
     void on();
+
+    /**
+     * Toggles the light state. 
+     */
     void toggle();
+
+    /**
+     * Gets the current value of the dimmed light.
+     *
+     * @return current light value.
+     */
     bool getState();
-    byte getValue();
-    void set(uint8_t value);
+
+    /**
+     * Gets the current state of the dimmed light.
+     *
+     * @return current light state.
+     */
+    uint8_t getValue();
+
+    /**
+     * Gets the current state of the dimmed light.
+     *
+     * @return current light value.
+     */
+    uint8_t getValue();
+
+    /**
+     * Sets the value and the state of the light.
+     * @param value 	the value of the dimm light. Values from 0 to 100.
+     * @param state 	the state of the light. ON or OFF.
+     */
+    void set(uint8_t value, uint8_t state = -1);
+
+  private:
+    uint8_t operation_mode;
+    uint8_t countResolution;
+    uint8_t halfCycleCounter;
+    uint8_t triacPin;
+    uint8_t lampValue;
+    uint8_t lampValue_ramp;
+    bool lampState;
+    unsigned int msCounter;
+
     void zeroCross();
     void triac();
-
-//private:
-    int start();
-    uint8_t operation_mode;
-
-    byte countResolution;
-    bool zeroCrosAttached;
-    
-    //Triac pin vector
-    byte triacPin;
-    
-    //Dimmer lamps value
-    byte lampValue;
-    
-    //Temporary lamps value
-    byte lampValue_ramp;
-    
-    //Virtual switch
-    byte lampState;
-    
-    //50ms Counter
-    unsigned int msCounter;
-    
-    // Half cycle counter
-    byte halfCycleCounter;
 };
 
 void callZeroCross();
--- examples/DisplayLamps/DisplayLamps.ino
+++ examples/DisplayLamps/DisplayLamps.ino
@@ -13,66 +13,34 @@
 #include <TimerOne.h>
 #include "Dimmer.h"
 
-Dimmer dimmer1;
+DimmerControl lamp1(3, 50, ON);
+DimmerControl lamp2(5, 50, OFF);
+DimmerControl lamp3(5, 50, OFF);
 
+//Light powers animation
 #define p1  10
 #define p2  40
-
-byte pot[6][3] = {  {p2,  0, 0 }, 
-                    {p1, p1, 0 },
-                    {0 , p2, 0 }, 
-                    {0 , p1, p1}, 
-                    {0 ,  0, p2},
-                    {p1,  0, p1}
-                  };
+byte pot[6][3] = {
+  {p2,  0, 0 },
+  {p1, p1, 0 },
+  {0 , p2, 0 },
+  {0 , p1, p1},
+  {0 ,  0, p2},
+  {p1,  0, p1}
+};
 
 void setup() {
-  dimmer1.attachZeroCross(2,0);
-  dimmer1.attachTriac(3);
-  dimmer1.attachTriac(5);
-  dimmer1.attachTriac(6);
-  dimmer1.init();
+  lamp1.begin(NORMAL_MODE);
+  lamp2.begin(RAMP_MODE);
+  lamp2.begin(NORMAL_MODE);
 }
 
-void loop(){
-  wave1(200, 3);
-  dimmer1.initRamp();
-  wave2(700, 3);
-  OnOff(500, 3);
-  dimmer1.init();
-  OnOff(300, 3);
-  OnOff(100, 3);
-}
-
-void wave1(short delayms, byte times){
-  for(byte k=0; k<times; k++){
-    for(byte i=0; i<6; i++){
-      for(byte j=0; j<3; j++){
-        dimmer1.set(j+1, pot[i][j]);
-      }
-      delay(delayms);
-    }
-  }
-}
-
-void wave2(short delayms, byte times){
-  for(byte k=0; k<times; k++){
-    for(byte i=0; i<6; i++){
-      for(byte j=0; j<3; j++){
-        dimmer1.set(j+1, pot[i][j]==p2 ? 0 : p2);
-      }
-      delay(delayms);
-    }
-  }
-}
-
-void OnOff(short delayms, byte times){
-  for(byte k=0; k<times; k++){
-    for(byte i=0; i<6; i++){
-      for(byte j=0; j<3; j++){
-        dimmer1.set(j+1, pot[i][j]==p2 ? p2 : 0);
-      }
-      delay(delayms);
-    }
+void loop() {
+  //Wave form animation
+  for (byte i = 0; i < 6; i++) {
+    lamp1.set(pot[i][0]);
+    lamp2.set(pot[i][1]);
+    lamp3.set(pot[i][2]);
+    delay(400);
   }
 }
