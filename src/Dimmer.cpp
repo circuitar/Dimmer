e8a83dbcd7e2de75d05d1836658b3f082ca790fc
Hamid Saffari <35362339+HamidSaffari@users.noreply.github.com>
2020-03-21 01:10:42 +0430

    added some new functions and STM32F1 Supported

--- src/Dimmer.cpp
+++ src/Dimmer.cpp
@@ -2,6 +2,7 @@
  * This is a library to control the intensity of dimmable AC lamps or other AC loads using triacs.
  *
  * Copyright (c) 2015 Circuitar
+ * Updated by hamidsaffari@yahoo.com @ 2020
  * This software is released under the MIT license. See the attached LICENSE file for details.
  */
 
